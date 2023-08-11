4de06eb6cffc38fd6e3eadf096737438e0e48440
Mohaned <mohanedjaknon@gmail.com>
2023-08-10 17:41:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#Include <stdio.h>
+#Include < stdio.h >
 /**
- * main - A program that uses puts function 
- * Return: 0  
- */ 
-int main (void) 
+ * main - A program that uses puts function
+ * Return: 0
+ */
+int main(void)
 {
-puts ("\ Programming is like building a multilingual puzzle");
+puts("\ Programming is like building a multilingual puzzle");
 return (0);
 }
