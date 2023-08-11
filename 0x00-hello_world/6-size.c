a0452fe8182630338fe66805e26d06edead504c3
Mohaned <mohanedjaknon@gmail.com>
2023-08-10 19:00:58 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,3 +11,5 @@ printf ("size of a int: %lu byte(s)\n", sizedof(int));
 printf ("size of a long int: %lu byte(s)\n", sizedof(long int));
 printf ("size of a long long int: %lu byte(s)\n", sizedof(long long int));
 printf ("size of a float: %lu byte(s)\n", sizedof(float));
+return (0);
+}
