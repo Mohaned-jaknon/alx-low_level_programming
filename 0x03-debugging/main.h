4ae76cc204c01f22ab55639440257ab0e2d34c31
Mohaned <mohanedjaknon@gmail.com>
2023-08-17 16:51:28 -0700

    update

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest-number(int a, int b, int c);
+int convert-day(int month, int day);
+void print-remaining-days(int momnth, int day, int year);
+
+#endif
