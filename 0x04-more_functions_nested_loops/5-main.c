8768d4685167a637c6175e588bc32fe796eee536
Mohaned <mohanedjaknon@gmail.com>
2023-08-20 14:41:29 -0700

    done

--- 0x04-more_functions_nested_loops/5-main.c
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 
 /**
  * main - check the code
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	more_numbers();
+	more_number();
 	return (0);
 }
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * more_numbers - func prints 10 times the numbers, from 0 to 14
  * followed by a new line
